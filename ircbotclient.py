8859b079dc2ddff32e89bfe524d28d28307f6a71
Lorenz Leitner <lorenz.leitner@student.tugraz.at>
2020-04-23 12:00:42 +0200

    -

--- ircbotclient.py
+++ ircbotclient.py
@@ -6,13 +6,13 @@ from ircbot import IRCBot
 
 def main():
     server = "irc.snoonet.org"  # server
-    channel = "##bot-testing"  # channel
-    # channel = "#linuxmasterrace"  # channel
+    # channel = "##bot-testing"  # channel
+    channel = "#linuxmasterrace"  # channel
     botnick = "muh_bot"  # bot nick
     password = os.environ['IRCPW']
     adminname = "Asmodean"  # admin IRC nickname
     exitcode = "Be gone " + botnick
-    command_prefix = '\\'
+    command_prefix = '%'
 
     ircbot = IRCBot(server, channel, botnick, password, adminname, exitcode,
                     command_prefix)
