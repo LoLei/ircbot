54752f18b2b5053f19049597add6c50f161f36dd
Lorenz Leitner <lrnz.ltnr@gmail.com>
2021-06-27 15:48:11 +0200

    Make config work in test environment

--- src/settings.py
+++ src/settings.py
@@ -1,16 +1,23 @@
 import os
+import sys
 from typing import Dict
 
 CONFIG: Dict[str, str] = dict()
 
-CONFIG['server'] = os.environ["SERVER"]
-CONFIG['channel'] = os.environ["CHANNEL"]
-CONFIG['bot_nick'] = os.environ["BOT_NICK"]
-CONFIG['password'] = os.environ["PASSWORD"]
-CONFIG['admin_name'] = os.environ["ADMIN_NAME"]
-CONFIG['exit_code'] = os.environ["EXIT_CODE"]
-CONFIG['command_prefix'] = os.environ["COMMAND_PREFIX"]
-CONFIG['user_db_message_log_size'] = os.environ["USER_DB_MESSAGE_LOG_SIZE"]
-CONFIG['stopwords'] = os.environ["STOPWORDS"]
-CONFIG['imgur_client_id'] = os.environ["IMGUR_CLIENT_ID"]
-CONFIG['imgur_client_secret'] = os.environ["IMGUR_CLIENT_SECRET"]
+CONFIG['server'] = os.environ.get("SERVER", "")
+CONFIG['channel'] = os.environ.get("CHANNEL", "")
+CONFIG['bot_nick'] = os.environ.get("BOT_NICK", "")
+CONFIG['password'] = os.environ.get("PASSWORD", "")
+CONFIG['admin_name'] = os.environ.get("ADMIN_NAME", "")
+CONFIG['exit_code'] = os.environ.get("EXIT_CODE", "")
+CONFIG['command_prefix'] = os.environ.get("COMMAND_PREFIX", "")
+CONFIG['user_db_message_log_size'] = os.environ.get("USER_DB_MESSAGE_LOG_SIZE", "1000")
+CONFIG['stopwords'] = os.environ.get("STOPWORDS", "")
+CONFIG['imgur_client_id'] = os.environ.get("IMGUR_CLIENT_ID", "")
+CONFIG['imgur_client_secret'] = os.environ.get("IMGUR_CLIENT_SECRET", "")
+
+if "CI" not in os.environ:
+    empties = [v[0] for v in CONFIG.items() if v[1] == ""]
+    if empties:
+        raise EnvironmentError(f"Unset environment variables: {empties}");
+
